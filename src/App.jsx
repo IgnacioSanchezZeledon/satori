63ed230145e323d32db658f231195bf0d2a400ad
Ignacio Sanchez <jose.sanchezzeledon@ucr.ac.cr>
2024-06-18 10:36:57 -0600

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/lander" element={<HomePage />} />
         </Routes>
       </Router>
     </div>
