d19f15a5d07974955e7a7777127dfb25f33153c2
Ignacio Sanchez <jose.sanchezzeledon@ucr.ac.cr>
2024-06-18 17:03:37 -0600

    Bug en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/lander" element={<HomePage />} />
         </Routes>
       </Router>
     </div>
